e3a79d5c519ff570a14469432b734745f5f48b5e
Veranika <smeyun@list.ru>
2024-03-02 00:37:02 +0300

    feat: add feedback write button

---
+++ src/components/FeedbacksContent/components/FeedbackWriteButton/FeedbackWriteButton.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { Button } from 'antd';
+
+import { FeedbackWriteModal } from '../FeedbackWriteModal';
+import { FeedbackResultFooterError } from '../FeedbackResultFooterError';
+import { FeedbacksRefetch } from '@components/FeedbacksContent/types/feedbacksTypes';
+import { ResultCustom } from '@components/ResultCustom';
+import { ModalCustom } from '@components/ui/ModalCustom';
+
+import { FeedbacksStatus } from '@constants/feedbacks/feedbacksConstants';
+
+export const FeedbackWriteButton = ({ refetch }: FeedbacksRefetch) => {
+    const [isModalFormVisible, setIsModalFormVisible] = useState(false);
+    const [modalResult, setModalResult] = useState('');
+
+    const toggleModalVisibility = () => {
+        setIsModalFormVisible(!isModalFormVisible);
+    };
+
+    const closeModalResult = () => {
+        setModalResult('');
+    };
+
+    return (
+        <>
+            <Button
+                type='primary'
+                size='large'
+                onClick={toggleModalVisibility}
+                data-test-id='write-review'
+            >
+                Написать отзыв
+            </Button>
+            <FeedbackWriteModal
+                isModalVisible={isModalFormVisible}
+                toggleModalVisibility={toggleModalVisibility}
+                refetch={refetch}
+                setIsModalResult={setModalResult}
+            />
+            <ModalCustom open={!!modalResult}>
+                {modalResult === FeedbacksStatus.STATUS_ERROR ? (
+                    <ResultCustom
+                        statusCode={FeedbacksStatus.STATUS_ERROR}
+                        footer={[
+                            <FeedbackResultFooterError
+                                key="result-footer-error"
+                                closeModalResult={closeModalResult}
+                                setIsModalFormVisible={setIsModalFormVisible}
+                            />,
+                        ]}
+                    />
+                ) : (
+                    <ResultCustom
+                        statusCode={FeedbacksStatus.STATUS_SUCCESS}
+                        onClick={closeModalResult}
+                    />
+                )}
+            </ModalCustom>
+        </>
+    );
+};
