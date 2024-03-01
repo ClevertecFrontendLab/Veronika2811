32aacf69553603f01261463c9ff2d1a93efcf268
Veranika <smeyun@list.ru>
2024-03-01 20:36:54 +0300

    fix: fix aliases types for easier imports

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
             '@pages': path.resolve(__dirname, 'src/pages'),
             '@redux': path.resolve(__dirname, 'src/redux'),
             '@routes': path.resolve(__dirname, 'src/routes'),
-            '@types': path.resolve(__dirname, 'src/types'),
+            '@src/types': path.resolve(__dirname, 'src/types'),
             '@utils': path.resolve(__dirname, 'src/utils'),
         },
     },
