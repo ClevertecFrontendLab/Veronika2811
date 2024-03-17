effa3ad8938152142e8720c5767d3929d9746e37
Veranika <smeyun@list.ru>
2024-03-17 23:08:36 +0300

    feat: add types training

---
+++ src/types/training/trainingApiDataTypes.ts
@@ -0,0 +1,38 @@
+import type { Moment } from 'moment';
+
+import { CurrentTraining } from '@components/CalendarCustom/types/currentTraining';
+
+import { ErrorTypes } from '../errorTypes';
+
+type TrainingParameters = {
+    repeat: boolean;
+    period: number;
+    jointTraining: boolean;
+    participants: [string];
+}
+
+export type ExercisesItem = {
+    name: string;
+    replays: number;
+    weight: number;
+    approaches: number;
+    _id?: string;
+    isImplementation?: boolean;
+}
+
+export type TrainingResponse = ErrorTypes & {
+    _id: string;
+    name: string;
+    date: string;
+    isImplementation: boolean;
+    userId: string;
+    parameters: TrainingParameters;
+    exercises: ExercisesItem[];
+};
+
+export type TrainingEditData = {
+    name: string | null,
+    date: Moment,
+    exercises: CurrentTraining[] | null,
+    isImplementation?: boolean,
+}
