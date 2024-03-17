f64dde17f2c6f532011dbfc3ae7d295741831b8c
Veranika <smeyun@list.ru>
2024-03-17 23:05:30 +0300

    feat: add prepare headers

--- src/redux/api/feedbacks.api.ts
+++ src/redux/api/feedbacks.api.ts
@@ -1,26 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { BASE_URL } from './constants/baseUrl';
+import prepareHeaders from './utils/prepareHeaders';
 
-import { RootState } from '@redux/store';
 import { ErrorTypes } from '@/types/errorTypes';
 import { Feedback, FeedbackData } from '@/types/feedbacks';
 
 export const feedbacksApi = createApi({
-    reducerPath: 'FEEDBACKS_SLICE',
+    reducerPath: 'FEEDBACKS_API',
     baseQuery: fetchBaseQuery({
-        baseUrl: `${BASE_URL}`,
+        baseUrl: BASE_URL,
         credentials: 'include',
-        prepareHeaders: (headers, { getState }) => {
-            const accessToken = (getState() as RootState).authSlice.accessToken;
-            if (!accessToken) {
-                throw new Error('Отсутствует токен доступа');
-            }
-
-            headers.set('Authorization', `Bearer ${accessToken}`);
-
-            return headers;
-        },
+        prepareHeaders,
     }),
     endpoints: (build) => ({
         getFeedbacks: build.query<Feedback[], void>({
