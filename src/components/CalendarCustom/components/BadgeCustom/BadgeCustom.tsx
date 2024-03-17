f9dd2768024313e5c59e72ac196c291fd39634ba
Veranika <smeyun@list.ru>
2024-03-17 23:12:49 +0300

    feat: add BadgeCustom

---
+++ src/components/CalendarCustom/components/BadgeCustom/BadgeCustom.tsx
@@ -0,0 +1,11 @@
+import { Badge } from 'antd';
+
+import { BADGE_COLOR } from '@components/CalendarCustom/constants/badgeColor';
+
+export const BadgeCustom = ({ name }: { name: string }) => {
+    const colorBadge = BADGE_COLOR[name] || '#EB2F96'
+
+    return (
+        <Badge color={colorBadge} text={name} />
+    );
+}
