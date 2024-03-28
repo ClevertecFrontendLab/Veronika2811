bc109e5ffbd81101af182cc79a9e4b76688458bb
Veranika <smeyun@list.ru>
2024-03-28 20:15:35 +0300

    feat: add profile page

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -1,1 +1,13 @@
-export const ProfilePage = () => <div>profile-page</div>;
+import { ProfileHeader } from '@components/page-headers/profile-header/profile-header';
+import { ProfileForm } from '@components/profile-form';
+
+import styles from './profile-page.module.less';
+
+export const ProfilePage = () => (
+    <div className={styles['profile-page']}>
+        <ProfileHeader />
+        <div className={styles['profile-content']}>
+            <ProfileForm />
+        </div>
+    </div>
+);
