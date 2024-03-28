694d3c9b45d2eeaf350d59dd2ab9a78f268397cb
Veranika <smeyun@list.ru>
2024-03-28 20:15:05 +0300

    feat: add not found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { LogoApp } from '@components/ui/logo-app';
-import { Typography } from 'antd';
+import { push } from 'redux-first-history';
+import { useAppDispatch } from '@hooks/redux-hooks';
+import { Paths } from '@routes/constants/router-paths';
+import { Button, Result } from 'antd';
 import { Content } from 'antd/lib/layout/layout';
 
 import styles from './not-found-page.module.less';
 
-export const NotFoundPage: React.FC = () => (
-    <Content className={styles['not-found-page']}>
-        <div>
-            <LogoApp size='md' />
-        </div>
-        <Typography.Title>Извините!</Typography.Title>
-        <Typography.Paragraph>Эта страница недоступна. Попробуйте позже.</Typography.Paragraph>
-    </Content>
-);
+export const NotFoundPage: React.FC = () => {
+    const dispatch = useAppDispatch();
+
+    const goToMainPage = () => dispatch(push(Paths.MAIN));
+
+    return (
+        <Content className={styles['not-found-page']}>
+            <div className={styles['not-found-page-content']}>
+                <Result
+                    status='404'
+                    title='Такой страницы нет'
+                    subTitle='Извините, страница не найдена, возможно, она была удалена или перемещена.'
+                    extra={
+                        <Button type='primary' size='large' onClick={goToMainPage}>
+                            На главную
+                        </Button>
+                    }
+                />
+            </div>
+        </Content>
+    );
+};
